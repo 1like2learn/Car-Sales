c38359ea54480d5cf7c2d559380938bf065179a3
Nathan DeSpain <nathandespain@Nathans-MacBook-Pro.local>
2020-07-14 13:14:06 -0700

    additionalFeature is connected

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { connect } from "react-redux"
 
+import { removeFeature } from "../actions/featuresActions"
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
       <button 
-        // onClick={removeFeature}
+        onClick={removeFeature}
       className="button">X</button>
       {props.feature.name}
     </li>
